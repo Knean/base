a042e06f9988596d41e000d6ef92cb5d6f888e83
Knean <mario009c@yahoo.com>
2018-01-14 09:39:32 +0000

    picture changes on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 $(function() {
-  // jQuery goes here...
-
+  images = ["images\\laptop-mobile_small.jpg","images\\laptop-on-table_small.jpg", "images\\people-office-group-team_small.jpg"]
+  // jQuery goes here..."""")
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  // $(".red-box").fvvvvvvadeOut(2000);
+
+  var i = 0+images.length;
+  
+  
+    $("#pics").on("click",function(){
+   
+      $(this).prop("src",images[i % images.length])
+      ++i;
+     
+      
+    })
+  $("#btn-click").click(function(event){
+    console.log(event)
+     alert("button was clicked");
+
+  })
+  /* $(".red-box").fadeTo(2000,0.5) */
+  $(".red-box").hover(function(){$(this).fadeTo(2000,0.1)},function(){$(this).stop()})
+  $(".green-box").hover(function(event)
+    {
+      $(this).html("i was hovered");   
+    })
 });
